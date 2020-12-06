b9da7760bbd4d3c02e0a3444e3b89e149269fcf5
Василий Кузнецов <vasya1679@mail.ru>
2020-12-06 16:18:22 +0300

    Add project files.

---
+++ Towers/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Towers
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.messageText = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(457, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.OnReset);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.OnExit);
+            // 
+            // messageText
+            // 
+            this.messageText.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.messageText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.messageText.Location = new System.Drawing.Point(0, 137);
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(457, 46);
+            this.messageText.TabIndex = 1;
+            this.messageText.Text = "label1";
+            this.messageText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 183);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Башни";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseClick);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label messageText;
+    }
+}
+
---
+++ Towers/MainForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Towers
+{
+    public partial class MainForm : Form
+    {
+        
+        
+        private CellState[] cells = new CellState[] { CellState.White, CellState.White, CellState.White, CellState.Empty, CellState.Black, CellState.Black, CellState.Black };
+        private CellState[] winCells = new CellState[] { CellState.Black, CellState.Black, CellState.Black, CellState.Empty, CellState.White, CellState.White, CellState.White };
+        public MainForm()
+        {
+            InitializeComponent();
+            messageText.Text = string.Empty;
+        }
+        private void DrawCell(int cellIndex, Graphics gr)
+        {
+            var cellRect = GetCellRectangle(cellIndex);
+            //gr.DrawRectangle(Pens.Gray, cellRect);
+
+            if (cells[cellIndex] == CellState.White)
+            {
+                gr.FillEllipse(Brushes.White, cellRect.X + 2, cellRect.Y + 2, cellRect.Width - 4, cellRect.Height - 4);
+                gr.DrawEllipse(Pens.Black, cellRect.X + 2, cellRect.Y + 2, cellRect.Width - 4, cellRect.Height - 4);
+            }
+            else if (cells[cellIndex] == CellState.Black)
+            {
+                gr.FillEllipse(Brushes.Black, cellRect.X + 2, cellRect.Y + 2, cellRect.Width - 4, cellRect.Height - 4);
+            }
+
+
+
+        }
+        private Rectangle GetCellRectangle(int cellIndex)
+        {
+            var cellSize = (ClientSize.Width - 40) / 7;
+            var x = 5 + (cellIndex * (cellSize + 5));
+            var y = (ClientSize.Height - cellSize-messageText.Height) / 2;
+            return new Rectangle(x, y, cellSize, cellSize);
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                DrawCell(i, e.Graphics);
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+        private int GetEmptyCell(int cellIndex)
+        {
+            if (cells[cellIndex] == CellState.Empty) return -1;
+            if (cells[cellIndex] == CellState.White)
+            {
+                for (int i = 1; i < 3; i++)
+                {
+                    if (cellIndex + i >= cells.Length) break;
+                    if (cells[cellIndex + i] == CellState.Empty) return cellIndex + i;
+
+                }
+                return -1;
+            }
+            for (int i = 1; i < 3; i++)
+            {
+                if (cellIndex - i < 0) break;
+                if (cells[cellIndex - i] == CellState.Empty) return cellIndex - i;
+
+            }
+            return -1;
+        }
+
+        private void MainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if (GetCellRectangle(i).Contains(e.Location))
+                {
+                    int newIndex = GetEmptyCell(i);
+                    if (newIndex != -1)
+                    {
+                        cells[newIndex] = cells[i];
+                        cells[i] = CellState.Empty;
+                        Invalidate();
+                    }
+                    if (!CheckTurns())
+                    {
+                        if (string.Join(",", cells.Select(s => s.ToString()).ToArray()) == string.Join(",", winCells.Select(s => s.ToString()).ToArray()))
+                            messageText.Text = "Победа";
+                        else messageText.Text = "Нет ходов";
+                    }
+                    return;
+                }
+
+            }
+        }
+        private bool CheckTurns() => Enumerable.Range(0, 7).Any(i => GetEmptyCell(i) != -1);
+        
+
+        private void OnReset(object sender, EventArgs e)
+        {
+            cells = new CellState[] { CellState.White, CellState.White, CellState.White, CellState.Empty, CellState.Black, CellState.Black, CellState.Black };
+            messageText.Text = string.Empty;
+            Invalidate();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public enum CellState
+    {
+        Empty, White, Black
+    }
+    
+}
+
